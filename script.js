79e2d1f227aafeb4ca6f9cd2dccf185da3bd5340
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-02-17 22:43:56 +0530

    add feature to change number of grid elements

--- script.js
+++ script.js
@@ -1,43 +1,64 @@
 //$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##
 // DECLARATIONS
-const main = document.querySelector(".main");
-let boxWidth = 960;
-let numberOfSquares = 121;
-let sideLength = boxWidth/numberOfSquares - (.96*2);
+const box = document.querySelector(".box");
+const BOX_WIDTH = 960;
+const ELEMENT_BORDER_WIDTH = 0.96;
+let numberOfSquares = document.getElementById('square-number').value;
+// let numberOfSquares = 45;
+let sideLength = BOX_WIDTH/numberOfSquares - (ELEMENT_BORDER_WIDTH*2);
 
 //$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##
 // CONSOLE LOGS
 console.log(sideLength);
+console.log(numberOfSquares);
 
 
 //$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##
-// loop to create a line elment and add squares to the line
-// followed by adding line to the '.main' element
+function gridElementUpdate(){
+    let new_n = document.querySelector('#square-number').value;
+    numberOfSquares = new_n;
+    sideLength = BOX_WIDTH/numberOfSquares - (ELEMENT_BORDER_WIDTH*2);
+    clearGrid();
+    drawGrid(numberOfSquares);
+}
 
-for(let i=0; i < numberOfSquares; i++){
-    const lineElement = document.createElement('div');
-    lineElement.classList.add('line');
-    lineElement.id = 'l' + i;
-    for(let j=0; j < numberOfSquares; j++) {
-        const divEle = document.createElement("div");
-        divEle.classList.add('square');
-        divEle.id = 'sq' + j;
-        divEle.style.height = sideLength + 'px';
-        divEle.style.width = sideLength + 'px';
-        lineElement.appendChild(divEle);
+//$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##
+function clearGrid(){
+    while(box.firstChild) {
+        box.removeChild(box.lastChild);
     }
-    main.appendChild(lineElement)
+}
+//$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##
+// loop to create a line elment and add squares to the line
+// followed by adding line to the '.box' element
+function drawGrid(n_square) {
+    for(let i=0; i < n_square; i++){
+        const lineElement = document.createElement('div');
+        lineElement.classList.add('line');
+        lineElement.id = 'l' + i;
+        for(let j=0; j < n_square; j++) {
+            const divEle = document.createElement("div");
+            divEle.classList.add('square');
+            divEle.id = 'sq' + j;
+            divEle.style.height = sideLength + 'px';
+            divEle.style.width = sideLength + 'px';
+            lineElement.appendChild(divEle);
+        }
+        box.appendChild(lineElement)
+    }
 }
 //$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##$$##
 // dispaly cursor postion 
 
-main.addEventListener('mousemove', (e) => {
+box.addEventListener('mousemove', (e) => {
     let log = document.querySelector('#log');
     log.innerText = `
     Screen X/Y: (${e.screenX}, ${e.screenY})
     Client X/Y: (${e.clientX}, ${e.clientY})`
     const elementMouseIsOver = document.elementFromPoint(e.clientX, e.clientY);
-    console.log(elementMouseIsOver);
+    // #debug
+    // console.log(elementMouseIsOver);
     elementMouseIsOver.style.backgroundColor = 'red';
 });
 
+drawGrid(42);
